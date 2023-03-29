b0a26e8fc5da29033ffcbacaea423cc84eda8548
just-vita <1254581982@qq.com>
2023-03-29 12:48:57 +0800

    1143. 最长公共子序列

--- src/top/vita/dp/DPQuestion.java
+++ src/top/vita/dp/DPQuestion.java
@@ -450,8 +450,26 @@ public class DPQuestion {
 		return res;
 	}
 
+	/**
+	 * 1143. 最长公共子序列
+	 */
+	public int longestCommonSubsequence(String text1, String text2) {
+		int[][] dp = new int[text1.length() + 1][text2.length() + 1];
 
-
+		for (int i = 1; i <= text1.length(); i++){
+			// 直接从i - 1和j - 1开始，省去初始化操作
+			char ch1 = text1.charAt(i - 1);
+			for (int j = 1; j <= text2.length(); j++){
+				char ch2 = text2.charAt(j - 1);
+				if (ch1 == ch2){
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				}else{
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		return dp[text1.length()][text2.length()];
+	}
 
 
 
