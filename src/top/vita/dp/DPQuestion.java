d7525f5b47d1362842e8e9df626024cc71318cca
just-vita <1254581982@qq.com>
2023-03-29 12:49:58 +0800

    53. 最大子数组和

--- src/top/vita/dp/DPQuestion.java
+++ src/top/vita/dp/DPQuestion.java
@@ -488,8 +488,24 @@ public class DPQuestion {
 		return dp[nums1.length][nums2.length];
 	}
 
-
-
+	/**
+	 * 53. 最大子数组和
+	 */
+	public int maxSubArray(int[] nums) {
+		if (nums.length == 1){
+			return nums[0];
+		}
+		int[] dp = new int[nums.length];
+		dp[0] = nums[0];
+		int res = dp[0];
+		for (int i = 1; i < nums.length; i++){
+			dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+			if (res < dp[i]){
+				res = dp[i];
+			}
+		}
+		return res;
+	}
 
 
 
