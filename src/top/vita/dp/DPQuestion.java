f5f20c0d675fc1d4b3284f49956850a211486832
just-vita <1254581982@qq.com>
2023-03-30 13:09:43 +0800

    115. 不同的子序列

--- src/top/vita/dp/DPQuestion.java
+++ src/top/vita/dp/DPQuestion.java
@@ -532,6 +532,35 @@ public class DPQuestion {
         return dp[s.length()][t.length()] == s.length();
     }
 
+    /**
+     * 115. 不同的子序列
+     */
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++){
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++){
+            char ch1 = s.charAt(i - 1);
+            for (int j = 1; j <= m; j++){
+                char ch2 = t.charAt(j - 1);
+                if (ch1 == ch2){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+
+
+
+
+
 
 
 }
