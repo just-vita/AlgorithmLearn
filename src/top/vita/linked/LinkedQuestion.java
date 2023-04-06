74a753caa71b24c27770393375990cb4308f3ab5
just-vita <1254581982@qq.com>
2023-04-06 12:55:13 +0800

    142. 环形链表 II

--- src/top/vita/linked/LinkedQuestion.java
+++ src/top/vita/linked/LinkedQuestion.java
@@ -653,6 +653,28 @@ public class LinkedQuestion {
         return null;
     }
 
-
+	/**
+	 * 142. ª∑–Œ¡¥±Ì II
+	 */
+	public ListNode detectCycle2(ListNode head) {
+		if (head == null || head.next == null || head.next.next == null) {
+			return null;
+		}
+		ListNode slow = head.next;
+		ListNode fast = head.next.next;
+		while (slow != fast){
+			if (fast.next == null || fast.next.next == null) {
+				return null;
+			}
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		fast = head;
+		while (slow != fast){
+			slow = slow.next;
+			fast = fast.next;
+		}
+		return slow;
+	}
 }
 
