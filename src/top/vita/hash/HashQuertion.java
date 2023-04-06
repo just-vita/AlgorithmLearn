b2d7964427601be3b112f220e85c1629a18ee083
just-vita <1254581982@qq.com>
2023-04-06 12:54:30 +0800

    242. 有效的字母异位词

--- src/top/vita/hash/HashQuertion.java
+++ src/top/vita/hash/HashQuertion.java
@@ -74,4 +74,24 @@ public class HashQuertion {
         }
         return res;
     }
+
+    /**
+     * 242. 有效的字母异位词
+     */
+    public boolean isAnagram(String s, String t) {
+        int[] record = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            record[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++){
+            record[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++){
+            if (record[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
