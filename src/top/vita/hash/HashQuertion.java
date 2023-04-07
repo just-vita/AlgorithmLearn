eb9164580792b3145f1cd33c415e95ca7b5017b3
just-vita <1254581982@qq.com>
2023-04-07 18:00:56 +0800

    454. 四数相加 II

--- src/top/vita/hash/HashQuertion.java
+++ src/top/vita/hash/HashQuertion.java
@@ -94,4 +94,206 @@ public class HashQuertion {
         return true;
     }
 
+    /**
+     * 454. 四数相加 II
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++){
+            for (int j = 0; j < nums2.length; j++){
+                int sum = nums1[i] + nums2[j];
+                if (map.containsKey(sum)){
+                    map.put(sum, map.get(sum) + 1);
+                } else{
+                    map.put(sum, 1);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < nums3.length; i++){
+            for (int j = 0; j < nums4.length; j++){
+                int sum = nums3[i] + nums4[j];
+                if (map.containsKey(0 - sum)){
+                    count += map.get(0 - sum);
+                }
+            }
+        }
+        return count;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
