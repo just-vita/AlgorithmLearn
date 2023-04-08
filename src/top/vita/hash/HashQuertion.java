f66b78149bc02d31d3ccb5680e135e37b3bdb326
just-vita <1254581982@qq.com>
2023-04-08 16:04:07 +0800

    15. 三数之和

--- src/top/vita/hash/HashQuertion.java
+++ src/top/vita/hash/HashQuertion.java
@@ -1,7 +1,6 @@
 package top.vita.hash;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * @Author vita
@@ -139,9 +138,65 @@ public class HashQuertion {
         return true;
     }
 
+    /**
+     * 15. 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++){
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                int sum = nums[left] + nums[right];
+                if (sum == target){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[++left]);
+                    while (left < right && nums[right] == nums[--right]);
+                } else if (sum > target){
+                    right--;
+                } else{
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
 
-
-
+    public List<List<Integer>> threeSum2(int[] nums) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++){
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]){
+                        left++;
+                    };
+                    while (left < right && nums[right] == nums[right - 1]){
+                        right--;
+                    };
+                    left++;
+                    right--;
+                } else if (sum > 0){
+                    right--;
+                } else{
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
 
 
 
