925a553d1b5a63dc085eac21d3937e8bb65c707a
just-vita <1254581982@qq.com>
2023-04-08 16:15:20 +0800

    18. 四数之和

--- src/top/vita/hash/HashQuertion.java
+++ src/top/vita/hash/HashQuertion.java
@@ -198,8 +198,45 @@ public class HashQuertion {
         return res;
     }
 
+    /**
+     * 18. 四数之和
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++){
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++){
+                if (j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right){
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target){
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]){
+                            left++;
+                        };
+                        while (left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        };
+                        left++;
+                        right--;
+                    } else if (sum > target){
+                        right--;
+                    } else{
+                        left++;
+                    }
+                }
 
-
+            }
+        }
+        return res;
+    }
 
 
 
