beacf7ccd678d37bf1ad95e2003587279d6c49bc
just-vita <1254581982@qq.com>
2023-04-09 13:00:20 +0800

    344. 反转字符串

--- src/top/vita/string/StringQuestion.java
+++ src/top/vita/string/StringQuestion.java
@@ -777,9 +777,21 @@ public class StringQuestion {
 		}
 		return res;
 	}
-	
-	
-	
+
+	/**
+	 * 344. ·´×ª×Ö·û´®
+	 */
+	public void reverseString1(char[] s) {
+		int left = 0;
+		int right = s.length - 1;
+		while (left < right){
+			char tmp = s[left];
+			s[left] = s[right];
+			s[right] = tmp;
+			left++;
+			right--;
+		}
+	}
 	
 	
 	
