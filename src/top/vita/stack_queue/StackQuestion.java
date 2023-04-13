f54bac30348dcfd84cdfb52c7ff4abe63c641d6c
just-vita <1254581982@qq.com>
2023-04-13 21:27:04 +0800

    20. 有效的括号

--- src/top/vita/stack_queue/StackQuestion.java
+++ src/top/vita/stack_queue/StackQuestion.java
@@ -55,4 +55,37 @@ public class StackQuestion {
 		}
     	return p + 1;
     }
+
+	/**
+	 * 20. 有效的括号
+	 */
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (c == '('){
+				stack.push(')');
+			} else if (c == '['){
+				stack.push(']');
+			} else if (c == '{'){
+				stack.push('}');
+			} else if (stack.isEmpty() || stack.peek() != c){
+				// 遇到了右括号，但是栈是空的，代表右括号是多余的
+				// 不为空的话，如果栈顶元素与这个右括号的类型不同，也代表多余
+				return false;
+			} else{
+				// 遇到右括号了
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
+	}
+
+
+
+
+
+
+
+
+
 }
