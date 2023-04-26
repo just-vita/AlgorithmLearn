bd451899e575d5f725d43814ce8d973bbbe3f439
just-vita <1254581982@qq.com>
2023-04-26 12:37:47 +0800

    199. 二叉树的右视图

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -169,4 +169,29 @@ public class TreeQuestion {
         Collections.reverse(result);
         return result;
     }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if (i == size - 1){
+                    result.add(node.val);
+                }
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return result;
+    }
 }
