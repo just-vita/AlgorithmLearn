eff609e94953688946364c9646deef0839d3c180
just-vita <1254581982@qq.com>
2023-04-26 12:32:10 +0800

    94. 二叉树的中序遍历

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -11,11 +11,11 @@ import java.util.Stack;
 public class TreeQuestion {
     public List<Integer> preorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<>();
-        dfs(root, result);
+        dfs1(root, result);
         return result;
     }
 
-    public void dfs(TreeNode root, ArrayList<Integer> result) {
+    public void dfs1(TreeNode root, ArrayList<Integer> result) {
         if (root == null){
             return;
         }
@@ -40,6 +40,41 @@ public class TreeQuestion {
             }
             if (node.left != null){
                 stack.push(node.left);
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+
+    public void dfs(TreeNode root, ArrayList<Integer> result) {
+        if (root == null){
+            return;
+        }
+        dfs(root.left, result);
+        result.add(root.val);
+        dfs(root.right, result);
+    }
+
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()){
+            if (cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            } else{
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
             }
         }
         return result;
