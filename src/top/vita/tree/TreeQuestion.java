e51341c2dd812c889d563a70782868230c4e1fd3
just-vita <1254581982@qq.com>
2023-05-03 20:08:00 +0800

    110. 平衡二叉树

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -450,4 +450,26 @@ public class TreeQuestion {
         return treeNum;
     }
 
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = getHeight(root.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        // 左右子树高度差大于1，return -1表示已经不是平衡树了
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
