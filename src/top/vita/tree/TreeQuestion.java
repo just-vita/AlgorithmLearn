5c14fa480c104db8cf749f344cd2bf1eaa9a8678
just-vita <1254581982@qq.com>
2023-05-10 15:42:34 +0800

    700. 二叉搜索树中的搜索

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -728,7 +728,18 @@ public class TreeQuestion {
         return null;
     }
 
-
+    public TreeNode searchBST2(TreeNode root, int val) {
+        while (root != null){
+            if (val < root.val){
+                root = root.left;
+            } else if (val > root.val){
+                root = root.right;
+            } else{
+                return root;
+            }
+        }
+        return null;
+    }
 
 
 
