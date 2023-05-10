c30444549bc24745fa01a742aa50bdd547e673cf
just-vita <1254581982@qq.com>
2023-05-10 15:43:27 +0800

    617. 合并二叉树

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -755,7 +755,38 @@ public class TreeQuestion {
         return root1;
     }
 
-
+    public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+        if (root1 == null){
+            return root2;
+        }
+        if (root2 == null){
+            return root1;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root1);
+        queue.offer(root2);
+        while (!queue.isEmpty()){
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            node1.val += node2.val;
+            if (node1.left != null && node2.left != null){
+                queue.offer(node1.left);
+                queue.offer(node2.left);
+            }
+            if (node1.right != null && node2.right != null){
+                queue.offer(node1.right);
+                queue.offer(node2.right);
+            }
+            // 直接操作这棵树
+            if (node1.left == null){
+                node1.left = node2.left;
+            }
+            if (node1.right == null){
+                node1.right = node2.right;
+            }
+        }
+        return root1;
+    }
 
 
 
