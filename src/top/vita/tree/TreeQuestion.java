76795c238d0ea7a4ac5b10395e8c8f94ff4b98d4
just-vita <1254581982@qq.com>
2023-05-10 15:43:00 +0800

    617. 合并二叉树

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -741,7 +741,19 @@ public class TreeQuestion {
         return null;
     }
 
-
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        // 一方为空就用另一方，为null也无所谓
+        if (root1 == null){
+            return root2;
+        }
+        if (root2 == null){
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
 
 
 
