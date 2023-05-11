39b0dd0db0541d9539369d0fc42d1e3aa6a8dbaa
just-vita <1254581982@qq.com>
2023-05-11 12:32:48 +0800

    98. 验证二叉搜索树

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -940,7 +940,33 @@ public class TreeQuestion {
         return left && right;
     }
 
+    public boolean isValidBST2(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        TreeNode pre = null;
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (cur != null || !stack.isEmpty()){
+            if (cur != null){
+                stack.push(cur);
+                // ◊Û
+                cur = cur.left;
+            } else{
+                cur = stack.pop();
 
+                // ÷–
+                if (pre != null && pre.val >= cur.val){
+                    return false;
+                }
+                pre = cur;
+
+                // ”“
+                cur = cur.right;
+            }
+        }
+        return true;
+    }
 
 
 
