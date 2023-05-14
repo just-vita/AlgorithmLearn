720e7bd781853d50b085132aabd43b89230b7359
just-vita <1254581982@qq.com>
2023-05-14 20:11:54 +0800

    701. 二叉搜索树中的插入操作

--- src/top/vita/tree/TreeQuestion.java
+++ src/top/vita/tree/TreeQuestion.java
@@ -1077,5 +1077,27 @@ public class TreeQuestion {
         return root;
     }
 
+    public TreeNode insertIntoBST2(TreeNode root, int val) {
+        if (root == null){
+            return new TreeNode(val);
+        }
+        TreeNode cur = root;
+        TreeNode parent = root;
+        while (cur != null){
+            parent = cur;
+            if (cur.val > val) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        if (parent.val > val){
+            parent.left = new TreeNode(val);
+        } else {
+            parent.right = new TreeNode(val);
+        }
+        return root;
+    }
+
 
 }
