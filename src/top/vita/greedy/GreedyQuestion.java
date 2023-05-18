42c3cac6f10f4d569e1298809e161cf7f1c855d8
just-vita <1254581982@qq.com>
2023-05-18 20:24:24 +0800

    53. 最大子数组和

--- src/top/vita/greedy/GreedyQuestion.java
+++ src/top/vita/greedy/GreedyQuestion.java
@@ -41,7 +41,18 @@ public class GreedyQuestion {
         return res;
     }
 
-
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int curSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curSum += nums[i];
+            maxSum = Math.max(maxSum, curSum);
+            if (curSum < 0){
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
 
 
 
