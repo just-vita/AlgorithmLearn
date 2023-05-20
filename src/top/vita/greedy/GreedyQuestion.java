efc26d32361d44ac03ce1a79371983f2c8a5022a
just-vita <1254581982@qq.com>
2023-05-20 21:36:55 +0800

    55. 跳跃游戏

--- src/top/vita/greedy/GreedyQuestion.java
+++ src/top/vita/greedy/GreedyQuestion.java
@@ -62,6 +62,18 @@ public class GreedyQuestion {
         return res;
     }
 
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++){
+            if (dp[i - 1] < i){
+                return false;
+            }
+            dp[i] = Math.max(dp[i - 1], i + nums[i]);
+        }
+        return true;
+    }
 
 
 
