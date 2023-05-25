e28c83ab9dbe16028372f9e180064514fc08b980
just-vita <1254581982@qq.com>
2023-05-25 13:19:23 +0800

    剑指 Offer 57. 和为s的两个数字

--- src/top/vita/array/ArrayQuestion.java
+++ src/top/vita/array/ArrayQuestion.java
@@ -1187,6 +1187,21 @@ public class ArrayQuestion {
 		return res;
 	}
 
+	public int[] twoSum1(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+		while (left < right){
+			int sum = nums[left] + nums[right];
+			if (sum > target) {
+				right--;
+			} else if (sum < target){
+				left++;
+			} else {
+				return new int[]{nums[left], nums[right]};
+			}
+		}
+		return new int[0];
+	}
 
 
 
