00add942a2ccbe824a57b3255ba1764ad3ddcb77
just-vita <1254581982@qq.com>
2023-05-25 13:20:53 +0800

    剑指 Offer 58 - I. 翻转单词顺序

--- src/top/vita/string/StringQuestion.java
+++ src/top/vita/string/StringQuestion.java
@@ -927,6 +927,27 @@ public class StringQuestion {
 		}
 	}
 
+	public String reverseWords(String s) {
+		// 将传进来的字符串以空格拆分
+		String[] strings = s.trim().split(" ");
+		StringBuilder sb = new StringBuilder();
+		// 从后往前遍历
+		for (int i = strings.length - 1; i >= 0; i--) {
+			// 去除多出来的空格
+			if (strings[i].equals("")) {
+				continue;
+			}
+			// 到头了，append然后去空格
+			if (i == 0) {
+				sb.append(strings[i].trim());
+			} else {
+				// 怕有多余的空格，去掉，再加上去
+				sb.append(strings[i].trim()).append(" ");
+			}
+		}
+		// 输出String
+		return sb.toString();
+	}
 
 
 }
