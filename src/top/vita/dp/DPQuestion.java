531eceb229c861efbed19db62d5f26baaf618379
just-vita <1254581982@qq.com>
2023-07-05 11:26:06 +0800

    剑指 Offer 47. 礼物的最大价值

--- src/top/vita/dp/DPQuestion.java
+++ src/top/vita/dp/DPQuestion.java
@@ -752,7 +752,23 @@ public class DPQuestion {
 		return dp[n];
 	}
 
-
+	public int maxValue(int[][] grid) {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				if (i == 0) {
+					grid[i][j] = grid[i][j] + grid[i][j - 1];
+				} else if (j == 0) {
+					grid[i][j] = grid[i][j] + grid[i - 1][j];
+				} else {
+					grid[i][j] =  grid[i][j] + Math.max(grid[i][j - 1], grid[i - 1][j]);
+				}
+			}
+		}
+		return grid[grid.length - 1][grid[0].length - 1];
+	}
 
 
 
