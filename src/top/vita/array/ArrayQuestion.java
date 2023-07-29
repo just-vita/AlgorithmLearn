3ab7f3bd17c86edda57eecef0b76dc4023e6ef10
just-vita <1254581982@qq.com>
2023-07-29 21:33:47 +0800

    128. 最长连续序列

--- src/top/vita/array/ArrayQuestion.java
+++ src/top/vita/array/ArrayQuestion.java
@@ -1361,7 +1361,25 @@ public class ArrayQuestion {
 		return numbers[left];
 	}
 
-
+	public int longestConsecutive(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		Arrays.sort(nums);
+		int count = 1;
+		int max = 1;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] + 1 == nums[i + 1]) {
+				count++;
+			} else if (nums[i] == nums[i + 1]) {
+				continue;
+			} else {
+				count = 1;
+			}
+			max = Math.max(max, count);
+		}
+		return max;
+	}
 
 
 
