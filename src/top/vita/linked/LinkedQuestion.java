6eb98061b309f579d13295a117745bd0f22c1cb3
just-vita <1254581982@qq.com>
2023-07-29 22:13:51 +0800

    141. 环形链表

--- src/top/vita/linked/LinkedQuestion.java
+++ src/top/vita/linked/LinkedQuestion.java
@@ -10,6 +10,22 @@ public class LinkedQuestion {
 
 	}
 
+	public boolean hasCycle1(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (slow != fast) {
+			if (fast == null || fast.next == null) {
+				return false;
+			}
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return true;
+	}
+
 	/*
 	 * 206. 反转链表 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
 	 */
