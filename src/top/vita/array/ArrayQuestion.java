5c46cfc642fbf0b3e0c9190abe6f75a9165a6a70
just-vita <1254581982@qq.com>
2023-07-31 22:32:55 +0800

    11. 盛最多水的容器

--- src/top/vita/array/ArrayQuestion.java
+++ src/top/vita/array/ArrayQuestion.java
@@ -1397,4 +1397,19 @@ public class ArrayQuestion {
 		}
 	}
 
+	public int maxArea1(int[] height) {
+		int left = 0;
+		int right = height.length - 1;
+		int max = 0;
+		while (left < right) {
+			int area = Math.min(height[left], height[right]) * (right - left);
+			max = Math.max(max, area);
+			if (height[left] < height[right]) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return max;
+	}
 }
