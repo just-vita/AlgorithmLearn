5da09512e9c01a9259bb5ae09a87246b3027cef8
just-vita <1254581982@qq.com>
2023-08-13 23:15:23 +0800

    73. 矩阵置零

--- src/main/java/top/vita/graph/GraphQuestion.java
+++ src/main/java/top/vita/graph/GraphQuestion.java
@@ -759,52 +759,74 @@ public class GraphQuestion {
 			path.remove(path.size() - 1);
 		}
 	}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-	
-	
-	
-	
-	
+
+	public void setZeroes(int[][] matrix) {
+    	// 标记当前行列有没有零存在
+		int[] col = new int[matrix.length];
+		int[] row = new int[matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == 0) {
+					col[i] = 1;
+					row[j] = 1;
+				}
+			}
+		}
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (col[i] == 1) {
+					matrix[i][j] = 0;
+				}
+				if (row[j] == 1) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
