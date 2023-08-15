d6e69975fb848073ea283c195b7c7c9827688a0f
just-vita <1254581982@qq.com>
2023-08-15 21:52:51 +0800

    160. 相交链表

--- src/main/java/top/vita/linked/LinkedQuestion.java
+++ src/main/java/top/vita/linked/LinkedQuestion.java
@@ -879,6 +879,17 @@ public class LinkedQuestion {
 		return header.next;
 	}
 
-
+	public ListNode getIntersectionNode12(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null){
+			return null;
+		}
+		ListNode cur1 = headA;
+		ListNode cur2 = headB;
+		while (cur1 != cur2) {
+			cur1 = cur1 == null ? headB : cur1.next;
+			cur2 = cur2 == null ? headA : cur2.next;
+		}
+		return cur1;
+	}
 }
 
