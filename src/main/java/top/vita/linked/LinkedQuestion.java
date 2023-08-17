f332af4c422cbbabaebf3164dc737f17cf06eb07
just-vita <1254581982@qq.com>
2023-08-17 17:21:36 +0800

    234. 回文链表

--- src/main/java/top/vita/linked/LinkedQuestion.java
+++ src/main/java/top/vita/linked/LinkedQuestion.java
@@ -964,5 +964,21 @@ public class LinkedQuestion {
 		}
 		return flag;
 	}
+
+	public boolean hasCycle12(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+		ListNode fast = head;
+		ListNode slow = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (fast == slow) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
