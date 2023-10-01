3c3b89a4e347df6e2dbc2c2a6781abe0a9054df5
just-vita <1254581982@qq.com>
2023-10-01 17:01:50 +0800

    121. 买卖股票的最佳时机

--- src/main/java/top/vita/dp/DPQuestion.java
+++ src/main/java/top/vita/dp/DPQuestion.java
@@ -1002,7 +1002,19 @@ public class DPQuestion {
 		return Math.max(dp1[n - 1], dp2[n - 1]);
 	}
 
-
+	public int maxProfit(int[] prices) {
+		int n = prices.length;
+		if (n <= 1) {
+			return 0;
+		}
+		int[][] dp = new int[n][2];
+		dp[0][0] = -prices[0];
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = Math.max(dp[i - 1][0], dp[i][0] - prices[i]);
+			dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+		}
+		return dp[n - 1][1];
+	}
 
 
 
