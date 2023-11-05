89e83f007f85bbd224159f4405b53a3fb1a5ccdb
just-vita <1254581982@qq.com>
2023-11-05 16:05:34 +0800

    187. 重复的DNA序列

--- src/main/java/top/vita/string/StringQuestion.java
+++ src/main/java/top/vita/string/StringQuestion.java
@@ -1086,4 +1086,17 @@ public class StringQuestion {
 		}
 		return a > b;
 	}
+
+	public List<String> findRepeatedDnaSequences(String s) {
+		ArrayList<String> res = new ArrayList<>();
+		HashMap<String, Integer> map = new HashMap<>();
+		for (int i = 0; i <= s.length() - 10; i++) {
+			String sub = s.substring(i, i + 10);
+			map.put(sub, map.getOrDefault(sub, 0) + 1);
+			if (map.get(sub) == 2) {
+				res.add(sub);
+			}
+		}
+		return res;
+	}
 }
